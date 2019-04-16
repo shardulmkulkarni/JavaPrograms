61c1dd43af5acf46bb7ed66b0cd68dbdef06ca37
Shardul Kulkarni <ShardulMKulkarni@gmail.com>
2019-04-16 11:27:25 -0500

    conting words in sentence

---
+++ src/tayari/CountingWordsInSentence.java
@@ -0,0 +1,35 @@
+package tayari;
+
+import java.util.HashMap;
+
+public class CountingWordsInSentence {
+
+	public static void main(String[] args) {
+
+		String sentence = "Is this a string which is a test for string";
+		
+		String[] worlds = sentence.split(" ");
+		
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+		for (int i = 0; i < worlds.length; i++) {
+
+			if (map.containsKey(worlds[i])) {
+				
+				int count = map.get(worlds[i]);
+				
+				map.put(worlds[i], count + 1);
+				
+			} else {
+				
+				map.put(worlds[i], 1);
+				
+			}
+
+		}
+
+		System.out.println(map);
+		
+	}
+
+}
